339133dead968548fd96d37eb68d472030e78ea3
Lars Nieuwenhuizen <lars@opengemeenten.nl>
2020-03-18 11:42:30 +0100

    feat: Ask to dump the current database state on stopping environment

--- bin/stop-environment.sh
+++ bin/stop-environment.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+read -p "Do you want to dump the current database state? this overrides previous dumped sql file y/N " -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+bin/dump-db.sh
+
+fi
+
 bin/docker-compose.sh down
